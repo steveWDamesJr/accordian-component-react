3103362bacf5b54357de2299df023926360b3226
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2023-09-25 20:27:10 -0500

    feature: Add accordion component body

--- src/AccordionComponent/Accordion.js
+++ src/AccordionComponent/Accordion.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import { React, useRef, useState } from 'react';
 import data from './AccordionData';
+import { RiArrowDropDownLine } from 'react-icons/ri';
 
 // accordionItem component
 
-const AccordionItem = () => {
+const AccordionItem = ({ question, answer, isOpen, onClick }) => {
+  const contentHeight = useRef()
   return(
-    <h1>Accordion</h1>
-  )
+    <div className="wrapper" >
+    <button className={`question-container ${isOpen ? 'active' : ''}`} onClick={onClick} >
+     <p className='question-content'>{question}</p>
+     <RiArrowDropDownLine className= {`arrow ${isOpen ? 'active' : ''}`} />
+    </button>
+
+    <div ref={contentHeight} className="answer-container" style={
+      isOpen
+      ? { height:  contentHeight.current.scrollHeight }
+      : { height: "0px" }
+     }>
+     <p className="answer-content">{answer}</p>
+   </div>
+  </div>
+ )
 }
 
 // main Accordion component
