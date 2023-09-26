9e5f92e10d9bc91632b146f1a8c3384a0e2479d5
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2023-09-25 19:22:00 -0500

    feature: Add accordionItem & Accordion components

---
+++ src/AccordionComponent/Accordion.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+// accordionItem component
+
+const AccordionItem = () => {
+  return(
+    <h1>Accordion</h1>
+  )
+}
+
+// main Accordion component
+
+const Accordion = () => {
+  return(
+    <div>
+      <AccordionItem />
+    </div>
+  )
+}
+
+export default Accordion;
